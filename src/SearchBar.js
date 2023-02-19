7b24ffb354642331aff7375278d346eedb470c96
“peterdh” <p.dehueck@gmail.com>
2023-02-19 07:54:08 +0000

    ...

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -23,8 +23,6 @@ const SearchBar = ({books, updateBooks}) => {
         setSearchResults(results);
         //console.log(searchResults);
 
-
-
       }).catch((error) => {
         console.log("error:", error)
       });
